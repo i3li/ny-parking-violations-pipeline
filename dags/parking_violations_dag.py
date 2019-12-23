62c25d33dbe0be1a8108780ba2ffc2e398f91d8f
Ali Za <i3li.xz@gmail.com>
2019-12-23 23:15:27 +0300

    refactor: Change the design of the dag to include second staging layer

--- dags/parking_violations_dag.py
+++ dags/parking_violations_dag.py
@@ -1,69 +1,136 @@
 from datetime import datetime
-# import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import StageToRedshiftOperator
 from airflow.models import Variable
+from airflow.operators import StageToRedshiftOperator
+from airflow.operators import LoadTableOperator
+from airflow.operators import DataQualityOperator
+from helpers import SqlQueries
 
-def stage_table_task(s3_key, task_id, table, data_format=None):
-    key = Variable.get(s3_key)
-    if data_format is None:
-        s3_to_redshift = StageToRedshiftOperator(
-            task_id=task_id,
-            table=table,
-            s3_bucket=s3_bucket,
-            s3_key=key,
-            dag=dag
-        )
-    else:
-        s3_to_redshift = StageToRedshiftOperator(
-            task_id=task_id,
-            table=table,
-            s3_bucket=s3_bucket,
-            s3_key=key,
-            data_format=data_format,
-            dag=dag
-        )
-    return s3_to_redshift
 
+s3_bucket = Variable.get('s3_violations_bucket')
 dag = DAG('parking_violations_dag',
           description='Load and transfor parking violations data in AWS Redshift with Airflow',
           start_date=datetime(2019, 12, 11),
           schedule_interval=None
         )
 
-s3_bucket = Variable.get('s3_violations_bucket')
+def stage_table_task(s3_key, task_id, table, data_format=None):
+    key = Variable.get(s3_key)
+    s3_to_redshift = StageToRedshiftOperator(
+        task_id=task_id,
+        table=table,
+        s3_bucket=s3_bucket,
+        s3_key=key,
+        data_format=data_format,
+        dag=dag
+    )
+    return s3_to_redshift
 
 start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
 
 stage_states_to_redshift = stage_table_task('s3_parking_violations_states_key', 'Stage_states', 'stage_state', "CSV IGNOREHEADER 1 DELIMITER ';'")
 stage_violation_codes_to_redshift = stage_table_task('s3_parking_violations_violation_codes_key', 'Stage_violation_codes', 'stage_violation_code')
-stage_vehicle_body_types_to_redshift = stage_table_task('s3_parking_violations_vehicle_body_types_key', 'Stage_vehicle_body_types', 'stage_vehicle_body_type')
-stage_vehicle_plate_types_to_redshift = stage_table_task('s3_parking_violations_vehicle_plate_types_key', 'Stage_vehicle_plate_types', 'stage_vehicle_plate_type')
-stage_vehicle_colors_to_redshift = stage_table_task('s3_parking_violations_vehicle_colors_key', 'Stage_vehicle_colors', 'stage_vehicle_color')
+stage_vehicle_body_types_to_redshift = stage_table_task('s3_parking_violations_vehicle_body_types_key', 'Stage_vehicle_body_types', 'stage_vehicle_body_type', f"JSON 's3://{s3_bucket}/{Variable.get('s3_parking_violations_vehicle_body_types_jsonpaths_key')}'")
+stage_vehicle_plate_types_to_redshift = stage_table_task('s3_parking_violations_vehicle_plate_types_key', 'Stage_vehicle_plate_types', 'stage_vehicle_plate_type', f"JSON 's3://{s3_bucket}/{Variable.get('s3_parking_violations_vehicle_plate_types_jsonpaths_key')}'")
+stage_vehicle_colors_to_redshift = stage_table_task('s3_parking_violations_vehicle_colors_key', 'Stage_vehicle_colors', 'stage_vehicle_color', f"JSON 's3://{s3_bucket}/{Variable.get('s3_parking_violations_vehicle_colors_jsonpaths_key')}'")
 stage_violations_to_redshift = stage_table_task('s3_parking_violations_key', 'Stage_violations', 'stage_violation', "CSV IGNOREHEADER 1 DELIMITER ';' ACCEPTANYDATE DATEFORMAT 'auto'")
-# FOR THE SAMPLE FILE (',' AND NOT ',')
+# FOR THE SAMPLE FILE (',' AND NOT ';')
 # stage_violations_to_redshift = stage_table_task('s3_parking_violations_key', 'Stage_violations', 'stage_violation', "CSV IGNOREHEADER 1 ACCEPTANYDATE DATEFORMAT 'auto'")
-stage_counties_to_redshift = stage_table_task('s3_parking_violations_counties_key', 'Stage_counties', 'stage_county')
-stage_issuing_agencies_to_redshift = stage_table_task('s3_parking_violations_issuing_agencies_key', 'Stage_issuing_agencies', 'stage_issuing_agency')
+stage_counties_to_redshift = stage_table_task('s3_parking_violations_counties_key', 'Stage_counties', 'stage_county', f"JSON 's3://{s3_bucket}/{Variable.get('s3_parking_violations_counties_jsonpaths_key')}'")
+stage_issuing_agencies_to_redshift = stage_table_task('s3_parking_violations_issuing_agencies_key', 'Stage_issuing_agencies', 'stage_issuing_agency', f"JSON 's3://{s3_bucket}/{Variable.get('s3_parking_violations_issuing_agencies_jsonpaths_key')}'")
+
+stage2_violations_to_redshift = LoadTableOperator(
+    task_id='Stage2_violations',
+    table='stage2_violation',
+    select_sql_stmt=SqlQueries.stage2_violation_table,
+    dag=dag
+)
 
 end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
 
-# Define dependencies
-start_operator >> stage_states_to_redshift
-start_operator >> stage_violation_codes_to_redshift
-start_operator >> stage_vehicle_body_types_to_redshift
-start_operator >> stage_vehicle_plate_types_to_redshift
-start_operator >> stage_vehicle_colors_to_redshift
-start_operator >> stage_violations_to_redshift
-start_operator >> stage_counties_to_redshift
-start_operator >> stage_issuing_agencies_to_redshift
+# load_violation_code = LoadTableOperator(
+#     task_id='Load_violation_code',
+#     table='violation_code',
+#     select_sql_stmt=SqlQueries.violation_code_table,
+#     dag=dag
+# )
+#
+# load_date = LoadTableOperator(
+#     task_id='Load_date',
+#     table='date',
+#     select_sql_stmt=SqlQueries.date_table,
+#     dag=dag
+# )
+#
+# load_time = LoadTableOperator(
+#     task_id='Load_time',
+#     table='time',
+#     select_sql_stmt=SqlQueries.time_table,
+#     dag=dag
+# )
+#
+# load_location = LoadTableOperator(
+#     task_id='Load_location',
+#     table='location',
+#     columns='street_code1, street_code2, street_code3, street_name, intersecting_street, precinct, county, house_number',
+#     select_sql_stmt=SqlQueries.location_table,
+#     dag=dag
+# )
+#
+# load_issuer = LoadTableOperator(
+#     task_id='Load_issuer',
+#     table='issuer',
+#     select_sql_stmt=SqlQueries.issuer_table,
+#     dag=dag
+# )
+#
+# load_vehicle = LoadTableOperator(
+#     task_id='Load_vehicle',
+#     table='vehicle',
+#     select_sql_stmt=SqlQueries.vehicle_table,
+#     dag=dag
+# )
+#
+# has_rows_checker = lambda records: len(records) == 1 and len(records[0]) == 1 and records[0][0] > 0
+# dimension_check = DataQualityOperator(
+#     task_id='Check_dimensions',
+#     postgres_conn_id='redshift',
+#     sql_stmts = [
+#         SqlQueries.count_stmt('violation_code'),
+#         SqlQueries.count_stmt('date'),
+#         SqlQueries.count_stmt('"time"'),
+#         SqlQueries.count_stmt('location'),
+#         SqlQueries.count_stmt('issuer'),
+#         SqlQueries.count_stmt('vehicle')
+#     ],
+#     result_checkers = [
+#         has_rows_checker, has_rows_checker,
+#         has_rows_checker, has_rows_checker,
+#         has_rows_checker, has_rows_checker
+#     ],
+#     dag=dag
+# )
 
-stage_states_to_redshift                >> end_operator
-stage_violation_codes_to_redshift       >> end_operator
-stage_vehicle_body_types_to_redshift    >> end_operator
-stage_vehicle_plate_types_to_redshift   >> end_operator
-stage_vehicle_colors_to_redshift        >> end_operator
-stage_violations_to_redshift            >> end_operator
-stage_counties_to_redshift              >> end_operator
-stage_issuing_agencies_to_redshift      >> end_operator
+# Define dependencies
+
+stage_operators = [
+    # stage_states_to_redshift, stage_violation_codes_to_redshift,
+    # stage_vehicle_body_types_to_redshift, stage_vehicle_plate_types_to_redshift,
+    # stage_vehicle_colors_to_redshift, stage_counties_to_redshift,
+    # stage_issuing_agencies_to_redshift, stage_violations_to_redshift
+]
+
+fact_operators = [
+#     DummyOperator(task_id='Load_violation', dag=dag)
+]
+
+dimension_operators = [
+# load_violation_code, load_date,
+# load_time, load_location,
+# load_issuer, load_vehicle
+]
+
+# start_operator >> stage_operators >> stage2_violations_to_redshift >> fact_operators + dimension_operators >> end_operator
+# start_operator >> stage_operators >> stage2_violations_to_redshift >> end_operator
+start_operator >> stage2_violations_to_redshift >> end_operator
--- plugins/helpers/sql_queries.py
+++ plugins/helpers/sql_queries.py
@@ -1,5 +1,5 @@
 class SqlQueries:
-    violation_code_table = """
+    violation_code_table = r"""
     SELECT DISTINCT (code),
         description,
         manhattan_amount,
@@ -7,7 +7,7 @@ class SqlQueries:
     FROM public.stage_violation_code
     """
 
-    date_table = """
+    date_table = r"""
         SELECT "date",
             EXTRACT(day from "date") AS day,
             EXTRACT(month from "date") AS month,
@@ -26,7 +26,7 @@ class SqlQueries:
         )
         """
 
-    time_table = """
+    time_table = r"""
         -- THERE ARE SOME BAD TIME. e.g., 2131P
         SELECT
         	time_24 / 100 AS hours,
@@ -57,7 +57,7 @@ class SqlQueries:
         )
     """
 
-    location_table = """
+    location_table = r"""
         SELECT DISTINCT
             street_code1,
             street_code2,
@@ -75,7 +75,7 @@ class SqlQueries:
         ON UPPER(SV.violation_county) = UPPER(SC.code)
     """
 
-    issuer_table = """
+    issuer_table = r"""
         SELECT DISTINCT
             issuer_code AS code,
             -- COALESCE is used here to replace NULLs with 'Unknown'
@@ -119,7 +119,7 @@ class SqlQueries:
         ORDER BY issue_date DESC
     """
 
-    vehicle_table = """
+    vehicle_table = r"""
         SELECT DISTINCT
             CONCAT(registration_state, plate_id) AS v_id,
             plate_id,
@@ -182,3 +182,115 @@ class SqlQueries:
         ORDER BY issue_date DESC
 
     """
+
+    stage2_violation_table = r"""
+    SELECT
+      summons_number,
+      plate_id,
+      SS.description AS registration_state,
+      SVPT.type AS plate_type,
+      issue_date,
+      COALESCE(
+        SVC.code,
+        99) AS violation_code,
+      COALESCE(
+        SVBT.type,
+        'Unknown') AS vehicle_body_type,
+      vehicle_make,
+      SIA.agency,
+      street_code1,
+      street_code2,
+      street_code3,
+      vehicle_expiration_date,
+      CASE WHEN violation_precinct = '0'
+      THEN
+      	CASE WHEN issuer_precinct = '0'
+        THEN NULL -- Both violation_precinct and issuer_precinct are missing
+        ELSE issuer_precinct -- Only violation_precinct is missing
+        END
+      ELSE violation_precinct -- violation_precinct is not missing
+      END AS violation_precinct,
+      CASE WHEN violation_time ~* '^((2[0-3])|[0-1][0-9])[0-5][0-9][AP]$' -- Cecking the time format
+      THEN
+      	CASE WHEN TO_NUMBER(violation_time, '9999') > 1259 OR TO_NUMBER(violation_time, '9999') < 0100
+        THEN SUBSTRING(violation_time, 1, 4) -- The time is written in 24hs format, just ignore (A, P)M.
+        ELSE -- The time is written in 12hs format, convert it to 24hs format
+        	TO_CHAR(
+              TO_TIMESTAMP(violation_time || 'M', 'HHMIAM'),
+              'HH24MI'
+             )
+        END
+      ELSE NULL
+      END AS violation_time,
+      CASE WHEN time_first_observed ~* '^((2[0-3])|[0-1][0-9])[0-5][0-9][AP]$' -- Cecking the time format
+      THEN
+      	CASE WHEN TO_NUMBER(time_first_observed, '9999') > 1259 OR TO_NUMBER(time_first_observed, '9999') < 0100
+        THEN SUBSTRING(time_first_observed, 1, 4) -- The time is written in 24hs format, just ignore (A, P)M.
+        ELSE -- The time is written in 12hs format, convert it to 24hs format
+        	TO_CHAR(
+              TO_TIMESTAMP(time_first_observed || 'M', 'HHMIAM'),
+              'HH24MI'
+             )
+        END
+      ELSE NULL
+      END AS time_first_observed,
+      COALESCE(
+        SC.county,
+        'Unknown') AS violation_county,
+      CASE WHEN violation_in_front_of_or_opposite ~* '^[IFO]$'
+      THEN
+      	CASE WHEN violation_in_front_of_or_opposite ~* 'F'
+        THEN 'InFront'
+        ELSE 'Opposite'
+        END
+      ELSE NULL
+      END AS violation_in_front_of_or_opposite,
+      COALESCE(
+        house_number,
+        'Unknown') AS house_number,
+      COALESCE(
+        street_name,
+        'Unknown') AS street_name,
+      intersecting_street,
+      COALESCE(
+        CASE WHEN REPLACE(date_first_observed, ',', '') ~* '^\\d{8}$'
+        THEN
+          TO_DATE(
+          REPLACE(date_first_observed, ',', ''),
+          'YYYYMMDD'
+        )
+        ELSE NULL
+        END,
+      issue_date) AS date_first_observed,
+      law_section,
+      COALESCE(
+        sub_division,
+      'Unknown') AS sub_division,
+      COALESCE(
+        SVCL.color,
+        'Unknown') AS vehicle_color,
+      CASE WHEN vehicle_year >= 1884 AND vehicle_year <= EXTRACT(year FROM CURRENT_DATE) + 2
+      THEN vehicle_year
+      ELSE NULL
+      END AS vehicle_year
+    FROM stage_violation SV
+    JOIN stage_state SS -- INNER JOIN (DROP UNKNOWNS)
+    ON UPPER(SS.state_code) = UPPER(SV.registration_state)
+    JOIN stage_vehicle_plate_type SVPT -- INNER JOIN (DROP UNKNOWNS)
+    ON UPPER(SVPT.code) = UPPER(SV.plate_type)
+    LEFT JOIN stage_violation_code SVC
+    ON SVC.code = SV.violation_code
+    LEFT JOIN stage_vehicle_body_type SVBT
+    ON UPPER(SVBT.code) = UPPER(SV.violation_code)
+    LEFT JOIN stage_issuing_agency SIA
+    ON UPPER(SIA.code) = UPPER(SV.issuing_agency)
+    LEFT JOIN stage_county SC
+    ON UPPER(SC.code) = UPPER(SV.violation_county)
+    LEFT JOIN stage_vehicle_color SVCL
+    ON UPPER(SVCL.code) = UPPER(SV.vehicle_color)
+    WHERE plate_id IS NOT NULL
+    """
+
+    @staticmethod
+    def count_stmt(table_name='table'):
+        return f'SELECT COUNT(*) FROM {table_name}'
